fe3e25a4b5e6c18e6f2b6e1c019c04a69eff7ea2
Yichen Yan <or.ange.l.evol@gmail.com>
2017-02-21 14:50:34 +0800

    init resp with default Yii template

---
+++ controllers/SiteController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class SiteController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
