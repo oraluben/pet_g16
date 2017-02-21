d10baadba10597a5cb81f3879379791b9c37f05d
Yichen Yan <or.ange.l.evol@gmail.com>
2017-02-21 15:22:59 +0800

    add angular asset

---
+++ assets/AngularAsset.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: easonyan
+ * Date: 2/21/2017
+ * Time: 3:19 PM
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+class AngularAsset extends AssetBundle
+{
+    public $js = [
+        '//cdn.bootcss.com/angular.js/1.6.2/angular.js',
+    ];
+}
