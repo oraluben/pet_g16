a927176e6e6d19f83e74d7b4ed2142680c4aa843
zhu <sei_zyq2013@126.com>
2017-02-23 10:50:32 +0800

    added Login as index.php

--- views/site/index.php
+++ views/site/index.php
@@ -4,6 +4,49 @@
 
 $this->title = 'My Yii Application';
 ?>
-<div class="site-index">
-    empty page
+<link rel="stylesheet" href="<?=Yii::getAlias('@web/css/lrtk.css')?>">
+<div id="login">
+    <div class="wrapper">
+        <div class="login">
+            <form action="/" method="post" class="container offset1 loginform">
+                <div id="owl-login">
+                    <div class="hand"></div>
+                    <div class="hand hand-r"></div>
+                    <div class="arms">
+                        <div class="arm"></div>
+                        <div class="arm arm-r"></div>
+                    </div>
+                </div>
+                <div class="pad">
+                    <input type="hidden" name="_csrf" value="9IAtUxV2CatyxHiK2LxzOsT6wtBE6h8BpzOmk=">
+                    <div class="control-group">
+                        <div class="controls">
+                            <label for="username" class="control-label fa fa-envelope"></label>
+                            <input id="username" type="email" name="email" placeholder="Username" tabindex="1" autofocus="autofocus" class="form-control input-medium">
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <div class="controls">
+                            <label for="password" class="control-label fa fa-asterisk"></label>
+                            <input id="password" type="password" name="password" placeholder="Password" tabindex="2" class="form-control input-medium">
+                        </div>
+                    </div>
+                </div>
+                <div class="form-actions"><a href="http://www.lanrentuku.com/" tabindex="5" class="btn pull-left btn-link text-muted">Forgot password?</a>
+                    <button type="submit" tabindex="4" class="btn btn-primary">Login</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <script src="<?=Yii::getAlias('@web/js/jquery.min.js')?>"></script>
+    <script>
+        $(function() {
+
+            $('#login #password').focus(function() {
+                $('#owl-login').addClass('password');
+            }).blur(function() {
+                $('#owl-login').removeClass('password');
+            });
+        });
+    </script>
 </div>
