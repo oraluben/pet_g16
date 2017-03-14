7599327bfaca140bd10e46c093e1f0d691029a42
zhu <sei_zyq2013@126.com>
2017-03-14 15:40:07 +0800

    reset db.php

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=pet',
     'username' => 'root',
-    'password' => '941105',
+    'password' => '',
     'charset' => 'utf8',
 ];
