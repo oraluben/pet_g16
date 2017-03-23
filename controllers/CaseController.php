3b0587503f9832b80efafd07092e256e9ea3771c
Yichen Yan <or.ange.l.evol@gmail.com>
2017-03-23 10:35:22 +0800

    prettify code structure

--- controllers/CaseController.php
+++ controllers/CaseController.php
@@ -9,12 +9,10 @@
 namespace app\controllers;
 
 
+use app\actions\ClassificationsAction;
 use app\actions\CreateCaseAction;
 use app\actions\GetCasesByClassificationAction;
-use app\models\PetCaseClassification;
-use Codeception\Module\Queue;
 use yii\filters\AccessControl;
-use yii\helpers\ArrayHelper;
 use yii\rest\Controller;
 
 class CaseController extends Controller
@@ -49,44 +47,7 @@ class CaseController extends Controller
         return [
             'create' => CreateCaseAction::className(),
             'cases-by-cl' => GetCasesByClassificationAction::className(),
+            'classifications' => ClassificationsAction::className(),
         ];
-    }
-
-    public function actionClassifications()
-    {
-        $table = PetCaseClassification::find()->all();
-        $res = [];
-        $tree = [];
-        $q = [];
-        foreach ($table as $c) {
-            /** @var PetCaseClassification $c */
-            if (!is_null($c->parent)) {
-                $_c = ArrayHelper::getValue($res, $c->parent, []);
-                $_c[] = [$c->id, $c->classification_name];
-                $res[$c->parent] = $_c;
-            } else {
-                $_root = [$c->id, $c->classification_name];
-                $tree[] = &$_root;
-                $q[] = &$_root;
-            }
-        }
-
-        while (!empty($q)) {
-            $c = &$q[0];
-            if (ArrayHelper::keyExists($c[0], $res)) {
-                $child_list = ArrayHelper::getValue($c, 2, []);
-                foreach (ArrayHelper::getValue($res, $c[0], []) as $_c) {
-                    $cur_value = [$_c[0], $_c[1]];
-                    $q[] = &$cur_value;
-                    $child_list[] = &$cur_value;
-                    unset($cur_value);
-                }
-                $c[2] = $child_list;
-            }
-            array_shift($q);
-        }
-
-//        return $tree;
-        return $table;
     }
 }
