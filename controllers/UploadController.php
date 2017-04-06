24e5fddd4c6327188fb7c80420dfa920909c2c76
Yichen Yan <or.ange.l.evol@gmail.com>
2017-04-06 09:11:52 +0800

    add DELETE method for image and video

--- controllers/UploadController.php
+++ controllers/UploadController.php
@@ -9,15 +9,54 @@
 namespace app\controllers;
 
 use app\actions\ImageUploadAction;
+use app\models\PetCaseUnitImage;
+use yii\filters\AccessControl;
 use yii\rest\Controller;
 
 class UploadController extends Controller
 {
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        unset($behaviors['contentNegotiator']['formats']['application/xml']);
+
+        $behaviors['access'] = [
+            'class' => AccessControl::className(),
+            'rules' => [
+                [
+                    'allow' => true,
+                    'actions' => ['image', 'video'],
+                    'verbs' => ['POST'],
+                ],
+                [
+                    'allow' => true,
+                    'actions' => ['delete-image', 'delete-video'],
+                    'verbs' => ['DELETE'],
+                ],
+            ],
+        ];
+
+        return $behaviors;
+    }
+
     public function actions()
     {
         return [
             'image' => ImageUploadAction::className(),
 //            'VideoUpload' => VideoUploadAction::className(),
+
         ];
+    }
+
+    public function actionDeleteImage($id)
+    {
+        $i = PetCaseUnitImage::deleteAll($id);
+    }
+
+    public function actionDeleteVideo($id)
+    {
+        $i = PetCaseUnitImage::deleteAll($id);
     }
 }
