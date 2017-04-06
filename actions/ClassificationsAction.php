89e17187f5f01a9227082993a6a1cb5ac983b0af
Yichen Yan <or.ange.l.evol@gmail.com>
2017-04-06 08:46:10 +0800

    temporarily remove pretty classification for bugs

--- actions/ClassificationsAction.php
+++ actions/ClassificationsAction.php
@@ -18,40 +18,40 @@ class ClassificationsAction extends Action
     public function run($pretty = false)
     {
         $table = PetCaseClassification::find()->all();
-        if ($pretty) {
-            $res = [];
-            $tree = [];
-            $q = [];
-            foreach ($table as $c) {
-                /** @var PetCaseClassification $c */
-                if (!is_null($c->parent)) {
-                    $_c = ArrayHelper::getValue($res, $c->parent, []);
-                    $_c[] = [$c->id, $c->classification_name];
-                    $res[$c->parent] = $_c;
-                } else {
-                    $_root = [$c->id, $c->classification_name];
-                    $tree[] = &$_root;
-                    $q[] = &$_root;
-                }
-            }
-
-            while (!empty($q)) {
-                $c = &$q[0];
-                if (ArrayHelper::keyExists($c[0], $res)) {
-                    $child_list = ArrayHelper::getValue($c, 2, []);
-                    foreach (ArrayHelper::getValue($res, $c[0], []) as $_c) {
-                        $cur_value = [$_c[0], $_c[1]];
-                        $q[] = &$cur_value;
-                        $child_list[] = &$cur_value;
-                        unset($cur_value);
-                    }
-                    $c[2] = $child_list;
-                }
-                array_shift($q);
-            }
-
-            return $tree;
-        }
+//        if ($pretty) {
+//            $res = [];
+//            $tree = [];
+//            $q = [];
+//            foreach ($table as $c) {
+//                /** @var PetCaseClassification $c */
+//                if (!is_null($c->parent)) {
+//                    $_c = ArrayHelper::getValue($res, $c->parent, []);
+//                    $_c[] = [$c->id, $c->classification_name];
+//                    $res[$c->parent] = $_c;
+//                } else {
+//                    $_root = [$c->id, $c->classification_name];
+//                    $tree[] = &$_root;
+//                    $q[] = &$_root;
+//                }
+//            }
+//
+//            while (!empty($q)) {
+//                $c = &$q[0];
+//                if (ArrayHelper::keyExists($c[0], $res)) {
+//                    $child_list = ArrayHelper::getValue($c, 2, []);
+//                    foreach (ArrayHelper::getValue($res, $c[0], []) as $_c) {
+//                        $cur_value = [$_c[0], $_c[1]];
+//                        $q[] = &$cur_value;
+//                        $child_list[] = &$cur_value;
+//                        unset($cur_value);
+//                    }
+//                    $c[2] = $child_list;
+//                }
+//                array_shift($q);
+//            }
+//
+//            return $tree;
+//        }
         return $table;
     }
 }
