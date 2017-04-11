f4e05a07bef9109c41a79894d778d2dcba728201
mikuchobits <mikuchobits@gmail.com>
2017-04-11 14:27:54 +0800

    Upload模块 v0.7

---
+++ migrations/m170411_062711_create_flow_department.php
@@ -0,0 +1,81 @@
+<?php
+
+use yii\db\Migration;
+
+class m170411_055131_create_flow_department extends Migration
+{
+    public function up()
+    {
+        $this->createTable('flow', [
+            'id' => $this->primaryKey(),
+            'flow_name' => $this->text(),
+            'flow_text' => $this->text(),
+            'function' => $this->integer()->notNull()->defaultValue(-1),
+            'department' => $this->integer()->notNull()->defaultValue(-1),
+        ]);
+        $this->createTable('flow_image', [
+            'id' => $this->primaryKey(),
+            'image_path' => $this->text(),
+            'image_info' => $this->text(),
+            'flow' => $this->integer()->notNull()->defaultValue(-1),
+        ]);
+        $this->createTable('flow_video', [
+            'id' => $this->primaryKey(),
+            'video_path' => $this->text(),
+            'video_info' => $this->text(),
+            'flow' => $this->integer()->notNull()->defaultValue(-1),
+        ]);
+        $this->createTable('department', [
+            'id' => $this->primaryKey(),
+            'department_name' => $this->text(),
+            'department_text' => $this->text(),
+        ]);
+
+        $this->addForeignKey(
+            'fk-department-department_id',
+            'flow',
+            'department',
+            'department',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+        $this->addForeignKey(
+            'fk-iflow-flow_id',
+            'flow_image',
+            'flow',
+            'flow',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+        $this->addForeignKey(
+            'fk-vflow-flow_id',
+            'flow_video',
+            'flow',
+            'flow',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+
+    }
+
+    public function down()
+    {
+        echo "m170411_055131_create_flow_department cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
